f6ec0c5ed743a604e03d046d1d9d3c8cb80a9ef4
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-07-27 19:25:10 -0300

    Aula 01

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let percussao_q = "Pom";
+let percussao_w = "Clap";
+let percussao_e = "Tim";
+let percussao_a = "Percurssão A";
+let percussao_s = "Percurssão S";
+let percussao_d = "Percurssão D";
+let percussao_z = "Percurssão Z";
+let percussao_x = "Percurssão X";
+let percussao_c = "Percurssão C";
+
+
+function tocaSom(som) {
+    console.log(som)
+    som.play()
+}
+
+
+
+
+
+
