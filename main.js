31c92d76a4449396cde0d247b6ba32cca1cb9d43
julio-cesar96 <julio_cbs96@hotmail.com>
2021-07-29 10:32:57 -0300

    Pegando os elementos com query selector

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+// Projeto: substituir as variáveis fixas pelo elementos do html
+const percursao_q = document.querySelector('#som_tecla-q');
+const percursao_w = document.querySelector('#som_tecla-w');
+const percursao_e = document.querySelector('#som_tecla-e');
+const percursao_a = document.querySelector('#som_tecla-a');
+const percursao_s = document.querySelector('#som_tecla-s');
+const percursao_d = document.querySelector('#som_tecla-d');
+const percursao_z = document.querySelector('#som_tecla-z');
+const percursao_x = document.querySelector('#som_tecla-x');
+const percursao_c = document.querySelector('#som_tecla-c');
 
-let percussao_q = "Pom";
-let percussao_w = "Clap";
-let percussao_e = "Tim";
-let percussao_a = "Percurssão A";
-let percussao_s = "Percurssão S";
-let percussao_d = "Percurssão D";
-let percussao_z = "Percurssão Z";
-let percussao_x = "Percurssão X";
-let percussao_c = "Percurssão C";
+percursao_w.onclick = tocaSom(percursao_w);
+
+//Projeto: substituir uma variavel para cada som para usar uma lista com todos
+const todosOsSons = document.querySelectorAll('audio');
+
+
+
 
 
 function tocaSom(som) {
-    console.log(som)
-    som.play()
+    console.log(som);
+    som.play();
 }
 
 
