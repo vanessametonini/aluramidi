9ada3466311b5ad27ed60167f2963e83cc34f106
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-07-29 16:38:54 -0300

    Código comentado das primeiras aulas

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+// Primeiras aulas
+// const tecla = document.querySelector('.tecla_q');
 
+// tecla.addEventListener('click', () => {
+//     const idDoAudio = `som_${tecla.classList[1]}`;
+//     const tagAudio = document.querySelector(`#${idDoAudio}`);
+
+//     if(tagAudio) {
+//         tagAudio.play();
+//     }
+
+// })
+
+//Código da última aula
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas.forEach(function(tecla){
