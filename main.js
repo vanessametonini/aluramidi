542619d9bef286d9cf45d726d8949767998e8a90
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-10-06 17:11:16 -0300

    Atualizado projeto conforme video aulas

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-    const elementoAudio = document.querySelector(idElementoAudio);
-
-    if(elementoAudio){
-        elementoAudio.play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
     }
     else {
-        alert('Som não encontrado!')
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
     }
 
 }
@@ -24,11 +24,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
-    tecla.onkeyup = function() {
+    tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
 
